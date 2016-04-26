1abb992bdb09bc62d59efb1c51f1ffc263103eb7
Joseph Lindsay <jmlslc@gmail.com>
2016-04-26 11:15:03 -0600

    Initial Project Creation

---
+++ CollegeCourses/CollegeCourses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CollegeCourses
+{
+    public class CollegeCoursesApplication
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter courses here...");
+            string input = Console.ReadLine();
+            string[] inputArray;
+
+            // get user input or assign default value
+            if (input == null)
+                inputArray = ApplicationLogic.courses;
+            else
+            {
+
+            }
+            Console.Read();
+        }
+    }
+
+    public static class ApplicationLogic
+    {
+        public static string[] courses = new string[]
+        {
+            "Introduction to Paper Airplanes: ",
+            "Advanced Throwing Techniques: Introduction to Paper Airplanes",
+            "History of Cubicle Siege Engines: Rubber Band Catapults 101",
+            "Advanced Office Warfare: History of Cubicle Siege Engines",
+            "Rubber Band Catapults 101: ",
+            "Paper Jet Engines: Introduction to Paper Airplanes"
+        };
+
+
+    }
+}
---
+++ CollegeCourses/CollegeCoursesTest/CollegeCoursesUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using CollegeCourses;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CollegeCoursesTest
+{
+    [TestClass]
+    public class CollegeCoursesUnitTests
+    {
+        [TestMethod]
+        public void TestCourseNameHasProperFormat()
+        {
+            string[] inputArray = ApplicationLogic.courses;
+            Assert.IsTrue(IsValid(inputArray));
+        }
+
+        private bool IsValid(string[] inputArray)
+        {
+            return true;
+        }
+    }
+}
