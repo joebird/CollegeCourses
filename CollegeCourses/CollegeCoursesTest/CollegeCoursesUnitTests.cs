19f90200f5c1ee009ff0dc025720ba6a8a161404
Joseph Lindsay <jmlslc@gmail.com>
2016-04-26 11:21:30 -0600

    Added TestCourseNameHasImproperFormat

--- CollegeCourses/CollegeCoursesTest/CollegeCoursesUnitTests.cs
+++ CollegeCourses/CollegeCoursesTest/CollegeCoursesUnitTests.cs
@@ -13,6 +13,13 @@ namespace CollegeCoursesTest
             Assert.IsTrue(IsValid(inputArray));
         }
 
+        [TestMethod]
+        public void TestCourseNameHasImproperFormat()
+        {
+            string[] inputArray = new string[] { "a", "b", "c" };
+            Assert.IsTrue(!IsValid(inputArray));
+        }
+
         private bool IsValid(string[] inputArray)
         {
             return true;
